80195f8b0a29e825dced2b3f9a23cd742b112319
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-10-03 12:31:22 +0000

    feat: add function for reading element data from a folder
    This commit adds a new function `read_element_data` to the `element.py` file in the `utils` directory. The function reads element data from a specified folder. It retrieves a list of .csv files from the folder, excluding files with "Point" in their names. The function then reads the data from the first file in the list, obtaining the total counts and resolution. It iterates over the remaining files, reading their data and storing it in a dictionary container. Additionally, if a "Grey" element is not present in the container, the function checks for a .tif file with "Grey" in its name and includes it in the container if found. Lastly, the function converts the "Grey" data to a numpy array. The function returns the "Grey" data, included element names, the element data container, resolution, and total counts.

--- utils/element.py
+++ utils/element.py
@@ -5,6 +5,63 @@ import pandas as pd
 from PIL import Image
 
 
+def read_element_data(folder_full_path):
+    """Read element data from a folder."""
+    included_elements = []
+    file_list = [
+        f
+        for f in os.listdir(folder_full_path)
+        if f.endswith(".csv") and "Point" not in f
+    ]
+
+    if not file_list:
+        raise ValueError(f"Folder {folder_full_path} is empty")
+
+    temp_file_path = os.path.join(folder_full_path, file_list[0])
+    total_counts = data_read(temp_file_path)
+    resolution = obtain_resolution(temp_file_path)
+
+    element_data_container = {}
+    for file in file_list[1:]:
+        temp_file_path = os.path.join(folder_full_path, file)
+        temp_data = data_read(temp_file_path)
+
+        total_counts += temp_data
+        temp_element_name = file.split("_")[-1].split(".")[0].split(" ")[0]
+
+        if temp_element_name != "Grey":
+            included_elements.append(temp_element_name)
+
+        temp_data = remove_noise_data(temp_data)
+        element_data_container[temp_element_name] = temp_data
+
+    if "Grey" not in element_data_container:
+        file_list = [
+            f
+            for f in os.listdir(folder_full_path)
+            if f.endswith(".tif") and "Grey" in f
+        ]
+        if file_list:
+            grey_image = Image.open(
+                os.path.join(folder_full_path, file_list[0])
+            )
+            element_data_container["Grey"] = np.array(
+                grey_image, dtype=np.uint8
+            )
+
+    if "Grey" in element_data_container:
+        element_data_container["Grey"] = np.array(
+            element_data_container["Grey"], dtype=np.uint8
+        )
+
+    return (
+        element_data_container["Grey"],
+        included_elements,
+        element_data_container,
+        resolution,
+        total_counts,
+    )
+
 
 def data_read(file_path):
     if os.name == "posix":
