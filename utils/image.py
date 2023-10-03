6707d8451a9ac88789ee6c41ac29a1b1b35e482a
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-10-03 11:47:38 +0000

    Add image visualization utility function
    This commit adds a new utility function `imshowpair` to the `utils/image.py` module. The function allows displaying a pair of images side-by-side or overlaid using different visualization methods such as absolute difference, blending, checkerboard, or false color overlay. The function checks for equal dimensions of the input images and raises an exception if they are not the same.

---
+++ utils/image.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def imshowpair(A, B, method="diff"):
+    """
+    Display a pair of images side-by-side or overlaid.
+
+    Parameters:
+    - A, B: Input images
+    - method: Visualization method. Options are:
+        'diff': Display the absolute difference between A and B
+        'blend': Blend A and B using 50% of each
+        'checkerboard': Checkerboard pattern of A and B
+        'falsecolor': Overlay A and B with different colors
+    """
+    if A.shape != B.shape:
+        raise ValueError("Both images must have the same dimensions.")
+
+    if method == "diff":
+        output = np.abs(A - B)
+    elif method == "blend":
+        output = 0.5 * A + 0.5 * B
+    elif method == "checkerboard":
+        checker = np.tile(
+            np.array([[1, 0], [0, 1]]), (A.shape[0] // 2, A.shape[1] // 2)
+        )
+        output = np.where(checker == 1, A, B)
+    elif method == "falsecolor":
+        output = np.stack([A, B, np.zeros_like(A)], axis=-1)
+    else:
+        raise ValueError(f"Unknown method: {method}")
+
+    return output
+
+
+if __name__ == "__main__":
+    pass
