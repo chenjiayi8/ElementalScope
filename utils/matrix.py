7ce5e55c3895f1d101e84090fb0f228c1224be22
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-10-03 12:36:24 +0000

    feat: Add is_member function for checking element presence in matrices
    This commit adds a new function `is_member` to the `matrix.py` file in the `utils` directory. The `is_member` function checks if the elements of matrix A are present in matrix B, returning a boolean array and an index array. The function flattens the matrices, checks the presence of each element using `np.isin`, and finds the indices of the elements in matrix B using a list comprehension. Non-members are assigned an index of -1. The index array is then reshaped back to the original shape of matrix A.

--- utils/matrix.py
+++ utils/matrix.py
@@ -9,3 +9,25 @@ def shuffle_2d_array(array):
     return array_copy
 
 
+def is_member(matrix_a, matrix_b):
+    """This function checks if the elements of matrix A are present in matrix B.
+    It returns a boolean array and an index array."""
+    # Flatten the matrix
+    flat_a = matrix_a.ravel()
+    flat_b = matrix_b.ravel()
+
+    # Check presence
+    member = np.isin(flat_a, flat_b)
+
+    # Find location
+    index_flat = np.array(
+        [np.where(flat_b == a)[0][0] if a in flat_b else -1 for a in flat_a]
+    )
+
+    index_flat[~member] = -1  # Set indices of non-members to 0
+
+    # Reshape the index array to the original shape of A
+    index = index_flat.reshape(matrix_a.shape)
+
+    return member.reshape(matrix_a.shape), index
+
