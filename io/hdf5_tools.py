c76dba2ce4f34bfd506e1d99d38f441b13a2f54d
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-10-05 19:42:25 +0000

    Add function to write all h5py datasets
    This commit adds the `write_h5py_all_dataset` function to writes all datasets to an HDF5 file. The `write_h5py_all_dataset` function supports compression using GZIP.

--- io/hdf5_tools.py
+++ io/hdf5_tools.py
@@ -16,3 +16,21 @@ def read_h5py_all_dataset(file_path):
                 datasets[key] = value[...]  # Reads the entire dataset
         return datasets
 
+
+def write_h5py_all_dataset(file_path, datasets):
+    """Write all h5py datasets."""
+    with h5py.File(file_path, "a") as file:
+        for key, value in datasets.items():
+            if key in file:
+                del file[key]  # Delete the old dataset if it exists
+            if np.isscalar(value):
+                file.create_dataset(key, data=value)
+            else:
+                file.create_dataset(
+                    key,
+                    data=value,
+                    compression="gzip",
+                    compression_opts=DEFAULT_COMPRESSION_OPTS,
+                )
+
+
