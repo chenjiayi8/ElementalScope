9db1dd8863c4815c66ae5c5306c657f55e9f3af4
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-10-05 19:51:30 +0000

    Add ElementLoader class for loading data from element CSV file

--- io/data_tools.py
+++ io/data_tools.py
@@ -19,13 +19,49 @@ class HDF5Loader(QThread):
         self.full_path = None
 
     def set_task(self, folder_name, full_path):
+        """Set the task for the thread."""
         self.folder_name = folder_name
         self.full_path = full_path
 
     def run(self):
+        """Run the thread."""
         if not self.full_path:
             return
         data = read_h5py_all_dataset(self.full_path)
         self.data_loaded.emit((self.folder_name, data))
+
+
+class ElementLoader(QThread):
+    """This class loads the data from the element csv file."""
+
+    data_loaded = pyqtSignal(tuple)
+
+    def __init__(self):
+        super().__init__()
+        self.folder_name = None
+        self.full_path = None
+
+    def set_task(self, folder_name, full_path):
+        """Set the task for the thread."""
+        self.folder_name = folder_name
+        self.full_path = full_path
+
+    def run(self):
+        """Run the thread."""
+        if not self.full_path:
+            return
+        try:
+            (
+                _,
+                _,
+                data_container,
+                resolution,
+                _,
+            ) = read_element_data(self.full_path)
+            data_container["resolution"] = resolution
+        except ValueError as error:
+            self.data_loaded.emit((self.folder_name, error))
+            return
+        self.data_loaded.emit((self.folder_name, data_container))
 
 
